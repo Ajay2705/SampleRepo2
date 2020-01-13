22273b0750edd5dd99dcd91137856ef4d9e09ecb
Ajay2705 <ssvajay278@gmail.com>
2020-01-13 17:53:22 +0530

    first time commit

---
+++ src/test/java/Pack1/Sample1.java
@@ -0,0 +1,19 @@
+package Pack1;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+
+	@Test
+	public static void m() {
+		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("I am in Dummy 2");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+}
